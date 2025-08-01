e1e5be1e5c81649a3fadd28c5e13ac841408c61b
Paul Aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2025-08-01 13:00:54 -0700

    Remove CentOS dependencies image build container after build completion

--- presto/scripts/build_centos_deps_image.sh
+++ presto/scripts/build_centos_deps_image.sh
@@ -6,5 +6,6 @@ PATCH_FILE_PATH=$(readlink -f copy_arrow_patch.patch)
 
 pushd ../../../presto/presto-native-execution
 make submodules
-docker compose up centos-native-dependency
+docker compose up centos-native-dependency # Build dependencies image if there is none present.
+docker compose down centos-native-dependency
 popd
