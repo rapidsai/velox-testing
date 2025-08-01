dbcd08c90b2a650780a47328a82c163bf0ef49a9
Paul Aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2025-07-31 22:56:58 -0700

    Set NUM_THREADS based on nproc in scripts

--- presto/scripts/start_native_cpu_presto.sh
+++ presto/scripts/start_native_cpu_presto.sh
@@ -4,5 +4,5 @@ set -e
 
 ./stop_presto.sh
 ./build_centos_deps_image.sh
-docker compose -f ../docker/docker-compose.native-cpu.yml build --progress plain
+docker compose -f ../docker/docker-compose.native-cpu.yml build --build-arg NUM_THREADS=$(($(nproc) * 3 / 4)) --progress plain
 docker compose -f ../docker/docker-compose.native-cpu.yml up -d
--- presto/scripts/start_native_gpu_presto.sh
+++ presto/scripts/start_native_gpu_presto.sh
@@ -4,5 +4,5 @@ set -e
 
 ./stop_presto.sh
 ./build_centos_deps_image.sh
-docker compose -f ../docker/docker-compose.native-gpu.yml build --progress plain
+docker compose -f ../docker/docker-compose.native-gpu.yml build --build-arg NUM_THREADS=$(($(nproc) * 3 / 4)) --progress plain
 docker compose -f ../docker/docker-compose.native-gpu.yml up -d
