ae2304fb99569ee33da26a9716031e9f3a260841
misiugodfrey <misiug@nvidia.com>
2025-08-06 12:46:50 -0700

    hive-parquet connector

--- presto/scripts/start_native_gpu_presto.sh
+++ presto/scripts/start_native_gpu_presto.sh
@@ -6,3 +6,5 @@ set -e
 ./build_centos_deps_image.sh
 docker compose -f ../docker/docker-compose.native-gpu.yml build --build-arg NUM_THREADS=$(($(nproc) * 3 / 4)) --progress plain
 docker compose -f ../docker/docker-compose.native-gpu.yml up -d
+
+docker compose -f ../docker/docker-compose.native-gpu.yml exec presto-cli presto-cli --server presto-coordinator:8080 --catalog hive-parquet #--schema tpch_sf1
