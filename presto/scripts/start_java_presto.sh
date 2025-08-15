77c5a0f225e42486807994f1747e2e1c50de89b1
Matt Gara <570356+mattgara@users.noreply.github.com>
2025-08-15 15:15:09 -0700

    Revert "Velox: Setup build, testing and benchmark dockerized deployment"

--- presto/scripts/start_java_presto.sh
+++ presto/scripts/start_java_presto.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-# Validate repo layout using shared script
-../../scripts/validate_directories_exist.sh "../../../presto" "../../../velox"
-
 ./stop_presto.sh
 docker compose -f ../docker/docker-compose.java.yml up -d
