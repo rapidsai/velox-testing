60f742e23311930c849d8544227f750d1dd73d5e
misiugodfrey <misiug@nvidia.com>
2025-08-20 12:25:39 -0700

    Added scripts to generate tpch data via tpchgen-cli.  This includes one script to generate the data, and one to rewrite the parquet files.

---
+++ scripts/rewrite_parquet.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+import os
+import pyarrow.parquet as pq
+import pyarrow as pa
+import argparse
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+# Map all the known VARLEN columns to use DLBA encoding.
+column_encoding_map={'c_name':'DELTA_LENGTH_BYTE_ARRAY',
+                     'c_address':'DELTA_LENGTH_BYTE_ARRAY',
+                     'c_phone':'DELTA_LENGTH_BYTE_ARRAY',
+                     'c_mktsegment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'c_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'l_returnflag':'DELTA_LENGTH_BYTE_ARRAY',
+                     'l_linestatus':'DELTA_LENGTH_BYTE_ARRAY',
+                     'l_shipinstruct':'DELTA_LENGTH_BYTE_ARRAY',
+                     'l_shipmode':'DELTA_LENGTH_BYTE_ARRAY',
+                     'l_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'n_name':'DELTA_LENGTH_BYTE_ARRAY',
+                     'n_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'o_orderstatus':'DELTA_LENGTH_BYTE_ARRAY',
+                     'o_orderpriority':'DELTA_LENGTH_BYTE_ARRAY',
+                     'o_clerk':'DELTA_LENGTH_BYTE_ARRAY',
+                     'o_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'p_name':'DELTA_LENGTH_BYTE_ARRAY',
+                     'p_mfgr':'DELTA_LENGTH_BYTE_ARRAY',
+                     'p_brand':'DELTA_LENGTH_BYTE_ARRAY',
+                     'p_type':'DELTA_LENGTH_BYTE_ARRAY',
+                     'p_container':'DELTA_LENGTH_BYTE_ARRAY',
+                     'p_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'ps_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     'r_name':'DELTA_LENGTH_BYTE_ARRAY',
+                     'r_comment':'DELTA_LENGTH_BYTE_ARRAY',
+                     's_name':'DELTA_LENGTH_BYTE_ARRAY',
+                     's_address':'DELTA_LENGTH_BYTE_ARRAY',
+                     's_phone':'DELTA_LENGTH_BYTE_ARRAY',
+                     's_comment':'DELTA_LENGTH_BYTE_ARRAY'}
+
+# Optimal page size has been determined by experiment
+page_size=204800
+
+def process_file(input_file_path, output_dir, input_dir, verbose):
+    relative_path = os.path.relpath(os.path.dirname(input_file_path), input_dir)
+    output_file_path = os.path.join(output_dir, relative_path, os.path.basename(input_file_path))
+    
+    # Create output directory if it doesn't exist
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    
+    # Read the parquet file metadata
+    parquet_file = pq.ParquetFile(input_file_path)
+    schema = parquet_file.schema_arrow
+    
+    if verbose:
+        print(f"Converting {input_file_path} to {output_file_path}")
+
+    # Read the parquet file
+    table = pq.read_table(input_file_path)
+        
+    # Convert decimal columns to double
+    new_columns = []
+    for col in table.columns:
+        if hasattr(col.type, 'precision'):
+            col = col.cast(pa.float64())
+        new_columns.append(col)
+        
+    new_table = pa.Table.from_arrays(new_columns, schema.names)
+
+    # Write the table back to a parquet file
+    pq.write_table(new_table,
+                   output_file_path,
+                   data_page_size=page_size,
+                   use_dictionary=False, # False because we will custom encode dict columns.
+                   column_encoding=column_encoding_map)
+
+# Multi-thread file processing
+def process_dir(input_dir, output_dir, num_threads, verbose):
+    with ThreadPoolExecutor(num_threads) as executor:
+        futures = []
+        for root, _, files in os.walk(input_dir):
+            for file in files:
+                if file.endswith('.parquet'):
+                    input_file_path = os.path.join(root, file)
+                    futures.append(executor.submit(process_file, input_file_path, output_dir, input_dir, verbose))
+        for future in futures:
+            future.result()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Alter an exising directory of parquet files",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    
+    parser.add_argument('--input_dir', '-i', help='Path to input Parquet files' )
+    parser.add_argument('--output_dir', '-o', help='Path to output Parquet files')
+    parser.add_argument('--num_threads', '-n', help='Number of threads')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Verbose')
+
+    args = parser.parse_args()
+    process_dir(Path(args.input_dir), Path(args.output_dir), int(args.num_threads), bool(args.verbose))
---
+++ scripts/run_tpch.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+function parse_args() { 
+    while [[ $# -gt 0 ]]; do
+	case $1 in
+	    -h|--help)
+		echo "Generates a directory of TPCH data in parquet form.  Usage:"    
+		echo "$0 -s <scale_factor> -p <num_partitions> -o <output_dir> -t <temp_dir> -n <num_threads> -v (verbose?) -k (keep raw data?)"
+		exit 0
+		;;
+	    -s|--scale-factor)
+		if [[ -n $2 ]]; then
+		    SCALE_FACTOR=$2
+		    shift 2
+		else
+		    echo "Error: --scale-factor requires a value"
+		    exit 1
+		fi
+		;;
+	    -p|--partitions)
+		if [[ -n $2 ]]; then
+		    NUM_PARTITIONS=$2
+		    shift 2
+		else
+		    echo "Error: --num-partitions requires a value"
+		    exit 1
+		fi
+		;;
+	    -o|--output-dir)
+		if [[ -n $2 ]]; then
+		    OUTPUT_DIR=$2
+		    shift 2
+		else
+		    echo "Error: --output-dir requires a value"
+		    exit 1
+		fi
+		;;
+	    -t|--temp-dir)
+		if [[ -n $2 ]]; then
+		    TEMP_DIR=$2
+		    shift 2
+		else
+		    echo "Error: --temp-dir requires a value"
+		    exit 1
+		fi
+		;;
+	    -n|--num-threads)
+		if [[ -n $2 ]]; then
+		    NUM_THREADS=$2
+		    shift 2
+		else
+		    echo "Error: --num-threads requires a value"
+		    exit 1
+		fi
+		;;
+	    -v|--verbose)
+		VERBOSE=" -v"
+		shift 1
+		;;
+	    -k|--keep-raw)
+		KEEP_RAW=1
+		shift 1
+		;;
+	    *)
+		echo "Error: Unknown argument $1"
+		exit 1
+		;;
+	esac
+    done
+}
+
+function create_data() {
+    mkdir $TEMP_DIR
+    pushd $TEMP_DIR > /dev/null
+
+    # Create partitioned data (could parallelize).
+    for PART in `seq 1 $NUM_PARTITIONS`; do
+	[ ! -z $VERBOSE ] && echo "Creating partition $PART"
+        mkdir part-$PART
+	tpchgen-cli -s $SCALE_FACTOR --output-dir part-$PART --parts $NUM_PARTITIONS --part $PART --format=parquet &
+	# Limit concurrent jobs
+	if [[ $(jobs -r -p | wc -l) -ge $NUM_THREADS ]]; then
+	    wait -n # Wait for any one background job to finish
+	fi
+    done
+    wait
+
+    # Create output directory organized by directory for each table
+    for TABLE in customer lineitem nation orders part partsupp region supplier; do
+	mkdir $TABLE
+    done
+
+    # Move the partitioned data into the new directory structure.
+    for PART in `seq 1 $NUM_PARTITIONS`; do
+	for TABLE in customer lineitem nation orders part partsupp region supplier; do
+	    mv part-$PART/$TABLE.parquet $TABLE/$TABLE-$PART.parquet
+	done
+	rmdir part-$PART
+    done
+    popd > /dev/null
+    [ ! -z $VERBOSE ] && echo "Data created sucessfully"
+}
+
+# defaults
+SCALE_FACTOR=1
+NUM_PARTITIONS=1
+OUTPUT_DIR="data"
+TEMP_DIR="data_raw"
+VERBOSE=""
+
+parse_args "$@"
+
+[ -e $OUTPUT_DIR ] && echo "$OUTPUT_DIR already exists" && exit 1
+[ -e $TEMP_DIR ] && echo "$TEMP_DIR already exists" && exit 1
+[ ! -z $VERBOSE ] && echo "Starting data creation"
+( [ ! -z $VERBOSE ] && time create_data ) || create_data
+
+# Rewrite the files including:
+#   decimal -> double conversion
+#   page size adjustment
+#   dictionary encoding changes
+[ ! -z $VERBOSE ] && echo "Starting parquet file rewriting."
+REWRITE_ARGS="-i $TEMP_DIR -o $OUTPUT_DIR -n $NUM_THREADS $VERBOSE"
+( [ ! -z $VERBOSE ] && time ./rewrite_parquet.py $REWRITE_ARGS ) || ./rewrite_parquet.py $REWRITE_ARGS
+[ ! -z $VERBOSE ] && echo "Parquet files rewritten"
+
+# TODO: Add verification step (optional) that lists schema?
+
+[ -z $KEEP_RAW ] && echo "Removing raw data" && rm -rf $TEMP_DIR
