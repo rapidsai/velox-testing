360a663515fe250c0327b34bd30b15e67faec3c8
paul-aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2025-09-03 09:18:15 -0700

    Add option for building Presto CPU and GPU images without caching (#18)
    * Refactor start_native_cpu_presto.sh and start_native_gpu_presto.sh
    to use a shared helper script.
    
    * Add --no-cache option to start_native_cpu_presto.sh and
    start_native_gpu_presto.sh scripts.

--- presto/scripts/start_native_cpu_presto.sh
+++ presto/scripts/start_native_cpu_presto.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-set -e
-
-# Validate repo layout using shared script
-../../scripts/validate_directories_exist.sh "../../../presto" "../../../velox"
-
-./stop_presto.sh
-./build_centos_deps_image.sh
-docker compose -f ../docker/docker-compose.native-cpu.yml build --build-arg NUM_THREADS=$(($(nproc) * 3 / 4)) --progress plain
-docker compose -f ../docker/docker-compose.native-cpu.yml up -d
+DEVICE_TYPE=cpu SCRIPT_NAME=$0 ./start_native_presto_helper.sh "$@"
