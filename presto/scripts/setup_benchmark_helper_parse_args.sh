2acad19e3f34b3401f31ccbd1fcfa766570f8a7a
Paul Aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2025-09-09 08:46:56 -0700

    Add scripts that simplify setup of benchmark tables
    * Add new setup_benchmark_data_and_tables.sh and
    setup_benchmark_tables.sh scripts.
    
    * Add new docker bind mount for non-integration test data.
    
    * Refactor python scripts in order to enable reuse for
    both integration tests and benchmark table setup.
    
    * Add bash functions that enable python virtual environment
    setup and teardown using either venv or conda.
    
    * Add utility script for running python scripts within an
    isolated virtual environment.
    
    * Add benchmark tables setup section to README.

---
+++ presto/scripts/setup_benchmark_helper_parse_args.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z $SCRIPT_DESCRIPTION ]]; then
+  echo "Internal error: SCRIPT_DESCRIPTION must be set"
+  exit 1
+fi
+
+if [[ -z $SCRIPT_EXAMPLE_ARGS ]]; then
+  echo "Internal error: SCRIPT_EXAMPLE_ARGS must be set"
+  exit 1
+fi
+
+if [[ -z $PRESTO_DATA_DIR ]]; then
+  echo "Error: PRESTO_DATA_DIR must be set to the directory path that contains the benchmark data directories"
+  exit 1
+fi
+
+print_help() {
+  cat << EOF
+
+Usage: $0 [OPTIONS]
+
+$SCRIPT_DESCRIPTION
+
+OPTIONS:
+    -h, --help                          Show this help message.
+    -b, --benchmark-type                Type of benchmark to create tables for. Only "tpch" and "tpcds" are currently supported.
+    -s, --schema-name                   Name of the schema that will contain the created tables.
+    -d, --data-dir-name                 Name of the directory inside the PRESTO_DATA_DIR path for the benchmark data.
+    -c, --convert-decimals-to-floats    Convert all decimal columns to float column type.
+    $SCRIPT_EXTRA_OPTIONS_DESCRIPTION
+
+EXAMPLES:
+    $0 $SCRIPT_EXAMPLE_ARGS
+    $0 -h
+
+EOF
+}
+
+parse_args() { 
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -h|--help)
+        print_help
+        exit 0
+        ;;
+      -b|--benchmark-type)
+        if [[ -n $2 ]]; then
+          BENCHMARK_TYPE=$2
+          shift 2
+        else
+          echo "Error: --benchmark-type requires a value"
+          exit 1
+        fi
+        ;;
+      -s|--schema-name)
+        if [[ -n $2 ]]; then
+          SCHEMA_NAME=$2
+          shift 2
+        else
+          echo "Error: --schema-name requires a value"
+          exit 1
+        fi
+        ;;
+      -d|--data-dir-name)
+        if [[ -n $2 ]]; then
+          DATA_DIR_NAME=$2
+          shift 2
+        else
+          echo "Error: --data-dir-name requires a value"
+          exit 1
+        fi
+        ;;
+      -c|--convert-decimals-to-floats)
+        CONVERT_DECIMALS_TO_FLOATS_ARG="--convert-decimals-to-floats"
+        shift
+        ;;
+      *)
+        SCRIPT_EXTRA_OPTIONS_UNKNOWN_ARG=true
+        if [[ -n $SCRIPT_EXTRA_OPTIONS_PARSER ]]; then
+          $SCRIPT_EXTRA_OPTIONS_PARSER "$@"
+          if [[ "$SCRIPT_EXTRA_OPTIONS_UNKNOWN_ARG" == "false" ]]; then
+            shift $SCRIPT_EXTRA_OPTIONS_SHIFTS
+          fi
+        fi
+
+        if [[ "$SCRIPT_EXTRA_OPTIONS_UNKNOWN_ARG" == "true" ]]; then
+          echo "Error: Unknown argument $1"
+          print_help
+          exit 1
+        fi
+        ;;
+    esac
+  done
+}
+
+parse_args "$@"
+
+if [[ -z ${BENCHMARK_TYPE} || ! ${BENCHMARK_TYPE} =~ ^tpc(h|ds)$ ]]; then
+  echo "Error: A valid benchmark type (tpch or tpcds) is required. Use the -b or --benchmark-type argument."
+  print_help
+  exit 1
+fi
+
+if [[ -z ${SCHEMA_NAME} ]]; then
+  echo "Error: Schema name is required. Use the -s or --schema-name argument."
+  print_help
+  exit 1
+fi
+
+if [[ -z ${DATA_DIR_NAME} ]]; then
+  echo "Error: Data directory name is required. Use the -d or --data-dir-name argument."
+  print_help
+  exit 1
+fi
---
+++ presto/scripts/setup_benchmark_tables.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DESCRIPTION="This script sets up benchmark tables under the given schema name. The benchmark data 
+is expected to already exist under the PRESTO_DATA_DIR path in a directory with name 
+that matches the value set for the --data-dir-name argument."
+
+SCRIPT_EXAMPLE_ARGS="-b tpch -s my_tpch_sf100 -d sf100 -c"
+
+source ./setup_benchmark_helper_parse_args.sh
+
+if [[ ! -d ${PRESTO_DATA_DIR}/${DATA_DIR_NAME} ]]; then
+  echo "Error: Benchmark data must already exist inside: ${PRESTO_DATA_DIR}/${DATA_DIR_NAME}"
+  exit 1
+fi
+
+SCHEMA_GEN_SCRIPT_PATH=$(readlink -f ../../benchmark_data_tools/generate_table_schemas.py)
+CREATE_TABLES_SCRIPT_PATH=$(readlink -f ../../presto/testing/integration_tests/create_hive_tables.py)
+TEMP_SCHEMA_DIR=$(readlink -f temp-schema-dir)
+
+function cleanup() {
+  rm -rf $TEMP_SCHEMA_DIR
+}
+
+trap cleanup EXIT 
+
+../../scripts/run_py_script.sh -p $SCHEMA_GEN_SCRIPT_PATH --benchmark-type $BENCHMARK_TYPE \
+--schema-name $SCHEMA_NAME --schemas-dir-path $TEMP_SCHEMA_DIR $CONVERT_DECIMALS_TO_FLOATS_ARG
+
+../../scripts/run_py_script.sh -p $CREATE_TABLES_SCRIPT_PATH --schema-name $SCHEMA_NAME \
+--schemas-dir-path $TEMP_SCHEMA_DIR --data-dir-name $DATA_DIR_NAME
