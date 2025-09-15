9227744e6bad1ea6711c6283336ba56e33253671
Paul Aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2025-09-15 15:20:29 -0700

    Add support for running integration tests against existing schemas

--- presto/testing/integration_tests/common_fixtures.py
+++ presto/testing/integration_tests/common_fixtures.py
@@ -7,8 +7,12 @@ from . import test_utils
 @pytest.fixture(scope="module")
 def presto_cursor(request):
     benchmark_type = request.node.obj.BENCHMARK_TYPE
-    conn = prestodb.dbapi.connect(host="localhost", port=8080, user="test_user", catalog="hive",
-                                  schema=f"{benchmark_type}_test")
+    hostname = request.config.getoption("--hostname")
+    port = request.config.getoption("--port")
+    user = request.config.getoption("--user")
+    schema = request.config.getoption("--schema-name")
+    schema = schema if schema else f"{benchmark_type}_test"
+    conn = prestodb.dbapi.connect(host=hostname, port=port, user=user, catalog="hive", schema=schema)
     return conn.cursor()
 
 
@@ -16,9 +20,15 @@ def presto_cursor(request):
 def setup_and_teardown(request, presto_cursor):
     benchmark_type = request.node.obj.BENCHMARK_TYPE
     test_utils.init_duckdb_tables(benchmark_type)
-    schemas = test_utils.get_table_schemas(benchmark_type)
-    test_utils.create_tables(presto_cursor, schemas, benchmark_type)
+
+    should_create_tables = False if request.config.getoption("--schema-name") else True
+    if should_create_tables:
+        schemas = test_utils.get_table_schemas(benchmark_type)
+        test_utils.create_tables(presto_cursor, schemas, benchmark_type)
+
     yield
-    keep_tables = request.config.getoption("--keep-tables")
-    if not keep_tables:
-        test_utils.drop_tables(presto_cursor, schemas, benchmark_type)
+
+    if should_create_tables:
+        keep_tables = request.config.getoption("--keep-tables")
+        if not keep_tables:
+            test_utils.drop_tables(presto_cursor, schemas, benchmark_type)
