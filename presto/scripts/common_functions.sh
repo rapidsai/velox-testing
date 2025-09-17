9fc48cb9bd26a7e789bfa8bf667c17a80b5b9ebf
paul-aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2025-09-17 14:21:35 -0700

    Add support for running integration tests against existing schemas (#45)
    * Extend integration test script to allow testing against different
    hostnames, port numbers, usernames, and schemas.
    
    * Update integration test script to use functions in
    py_env_functions.sh.

--- presto/scripts/common_functions.sh
+++ presto/scripts/common_functions.sh
@@ -4,9 +4,13 @@ function wait_for_worker_node_registration() {
   trap "rm -rf node_response.json" RETURN
 
   echo "Waiting for a worker node to be registered..."
+  HOSTNAME=${1:-localhost}
+  PORT=${2:-8080}
+  COORDINATOR_URL=http://${HOSTNAME}:${PORT}
+  echo "Coordinator URL: $COORDINATOR_URL"
   local -r MAX_RETRIES=5
   local retry_count=0
-  until curl -s -f -o node_response.json http://localhost:8080/v1/node && \
+  until curl -s -f -o node_response.json ${COORDINATOR_URL}/v1/node && \
         (( $(jq length node_response.json) > 0 )); do
     if (( $retry_count >= $MAX_RETRIES )); then
       echo "Error: Worker node not registered"
--- presto/testing/integration_tests/common_fixtures.py
+++ presto/testing/integration_tests/common_fixtures.py
@@ -8,23 +8,41 @@ from . import test_utils
 @pytest.fixture(scope="module")
 def presto_cursor(request):
     benchmark_type = request.node.obj.BENCHMARK_TYPE
-    conn = prestodb.dbapi.connect(host="localhost", port=8080, user="test_user", catalog="hive",
-                                  schema=f"{benchmark_type}_test")
+    hostname = request.config.getoption("--hostname")
+    port = request.config.getoption("--port")
+    user = request.config.getoption("--user")
+    schema = request.config.getoption("--schema-name")
+    schema = schema if schema else f"{benchmark_type}_test"
+    conn = prestodb.dbapi.connect(host=hostname, port=port, user=user, catalog="hive", schema=schema)
     return conn.cursor()
 
 
 @pytest.fixture(scope="module")
 def setup_and_teardown(request, presto_cursor):
     benchmark_type = request.node.obj.BENCHMARK_TYPE
-    test_utils.init_duckdb_tables(benchmark_type)
 
-    schema_name = f"{benchmark_type}_test"
-    schemas_dir = test_utils.get_abs_file_path(f"schemas/{benchmark_type}")
-    data_sub_directory = f"integration_test/{benchmark_type}"
-    create_hive_tables.create_tables(presto_cursor, schema_name, schemas_dir, data_sub_directory)
+    has_schema_name = bool(request.config.getoption("--schema-name"))
+    scale_factor = request.config.getoption("--scale-factor")
+
+    if has_schema_name and not scale_factor:
+        raise pytest.UsageError("--scale-factor must be set when --schema-name is specified")
+
+    if scale_factor and not has_schema_name:
+        raise pytest.UsageError("--scale-factor should be set only when --schema-name is specified")
+
+    scale_factor = scale_factor if scale_factor else test_utils.get_scale_factor(benchmark_type)
+    test_utils.init_duckdb_tables(benchmark_type, scale_factor)
+
+    should_create_tables = not has_schema_name
+    if should_create_tables:
+        schema_name = f"{benchmark_type}_test"
+        schemas_dir = test_utils.get_abs_file_path(f"schemas/{benchmark_type}")
+        data_sub_directory = f"integration_test/{benchmark_type}"
+        create_hive_tables.create_tables(presto_cursor, schema_name, schemas_dir, data_sub_directory)
 
     yield
 
-    keep_tables = request.config.getoption("--keep-tables")
-    if not keep_tables:
-        create_hive_tables.drop_schema(presto_cursor, schema_name)
+    if should_create_tables:
+        keep_tables = request.config.getoption("--keep-tables")
+        if not keep_tables:
+            create_hive_tables.drop_schema(presto_cursor, schema_name)
