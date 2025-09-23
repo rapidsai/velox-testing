aa4289c4b5b6afe689638f2533b5c1bf92026f50
Simon Eves <seves@nvidia.com>
2025-09-22 17:43:54 -0700

    Revert "Add omit-Hadoop-SDK-download patch"
    This reverts commit 6ba843a55b665eb071b865d54ce657e91e234af5.

--- presto/scripts/build_centos_deps_image.sh
+++ presto/scripts/build_centos_deps_image.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-# patch deps image script to omit problematic Hadoop SDK download
-pushd ../../../presto
-git apply ../velox-testing/presto/scripts/omit_hadoop_sdk_install_patch.diff
-popd
+PATCH_FILE_PATH=$(readlink -f copy_arrow_patch.patch)
 
-# now build the deps image if needed
 pushd ../../../presto/presto-native-execution
 make submodules
 docker compose up centos-native-dependency # Build dependencies image if there is none present.
