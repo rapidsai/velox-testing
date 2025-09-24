7c6aff98ad0eab2b24e0aec5ba13ab9b3b229f64
Simon Eves <seves@nvidia.com>
2025-09-24 10:01:58 -0700

    Simplify to just library copy fix, per Paul

--- presto/scripts/build_centos_deps_image.sh
+++ presto/scripts/build_centos_deps_image.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-# patch Velox submodule for deps container build success
-pushd ../../../presto
-git apply ../velox-testing/presto/scripts/patch_hadoop_and_nvjitlink_092225.diff || true
-popd
-
 pushd ../../../presto/presto-native-execution
 make submodules
 docker compose up centos-native-dependency # Build dependencies image if there is none present.
