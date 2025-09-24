2ac8c6bfaa557b270b941b60fe5e16ee41ebbe15
Simon Eves <seves@nvidia.com>
2025-09-24 10:02:34 -0700

    Reinstate superfluous line for now

--- presto/scripts/build_centos_deps_image.sh
+++ presto/scripts/build_centos_deps_image.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+PATCH_FILE_PATH=$(readlink -f copy_arrow_patch.patch)
+
 pushd ../../../presto/presto-native-execution
 make submodules
 docker compose up centos-native-dependency # Build dependencies image if there is none present.
