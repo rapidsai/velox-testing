ae0330cb2ac97ab467c91dc70e3e13aaa5c537e1
Simon Eves <seves@nvidia.com>
2025-09-25 12:57:52 -0700

    Add arrow patch

--- presto/scripts/build_centos_deps_image.sh
+++ presto/scripts/build_centos_deps_image.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-# patch Velox submodule for deps container build success
-pushd ../../../presto/presto-native-execution/velox
-git apply ../../../velox-testing/presto/scripts/patch_hadoop_and_nvjitlink_092225.diff || true
+#
+# apply current patches for Presto deps container build success
+# as of 09/25/25
+#
+
+# in Presto, disable re-build of arrow
+pushd ../../../presto
+git apply ../velox-testing/presto/patches/patch_arrow_092525.diff || true
 popd
+# in Velox sub-module, change the Hadoop version and mirror, and add libnvjitlink install
+pushd ../../../presto/presto-native-execution/velox
+git apply ../../../velox-testing/presto/patches/patch_hadoop_and_nvjitlink_092225.diff || true
+popd
+
+#
+# build deps container
+#
 
 pushd ../../../presto/presto-native-execution
 make submodules
