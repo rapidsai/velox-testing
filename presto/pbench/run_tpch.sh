e3f3ca75bb7e0ef1ddd68af1a3559e5e1d580251
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2025-09-26 18:55:42 -0500

    restructured based on #48

---
+++ presto/pbench/run_tpch.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+BASE_DIR="$(dirname $(realpath $0))/.."
+OUTPUT_DIR="$BASE_DIR/pbench_output/tpch"
+PBENCH_DIR="$BASE_DIR/pbench/"
+COORD="localhost:8080"
+
+mkdir -p $OUTPUT_DIR
+$PBENCH_DIR/pbench run -s http://$COORD/ -o $OUTPUT_DIR $PBENCH_DIR/benchmarks/tpch/sf100.json 
