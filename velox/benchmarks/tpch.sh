a85c62ad2fd284a57522a135b6f6084fc8a3ca1c
Matt Gara <mgara@nvidia.com>
2025-10-14 14:53:06 -0700

    Allow memory resource override

--- velox/benchmarks/tpch.sh
+++ velox/benchmarks/tpch.sh
@@ -202,7 +202,7 @@ run_tpch_single_benchmark() {
       cudf_pass_read_limit=0
       BENCHMARK_EXECUTABLE="$(get_tpch_benchmark_executable_path "$device_type")"
       CUDF_FLAGS="--cudf_chunk_read_limit=${cudf_chunk_read_limit} --cudf_pass_read_limit=${cudf_pass_read_limit}"
-      VELOX_CUDF_FLAGS="--velox_cudf_enabled=true --velox_cudf_memory_resource=async"
+      VELOX_CUDF_FLAGS="--velox_cudf_enabled=true --velox_cudf_memory_resource=${VELOX_CUDF_MEMORY_RESOURCE:-async}"
       ;;
   esac
   
