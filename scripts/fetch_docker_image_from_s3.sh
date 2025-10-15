7150e2301a2c3245e215be6a7592307ca28139c6
Simon Eves <seves@nvidia.com>
2025-10-15 13:37:26 -0700

    Update S3 Fetch Script to allow use of simple personal/group AWS keys (#81)
    For the CI case, we use `assume-role` to obtain temporary rolling keys for the private bucket.
    For the dev case, we allow use of simple personal/group keys.

--- scripts/fetch_docker_image_from_s3.sh
+++ scripts/fetch_docker_image_from_s3.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+set -e
 
 #
 # fetch_docker_image_from_s3 <imagename> <subdir> <filename>
@@ -44,15 +44,16 @@ fetch_docker_image_from_s3() {
   local BUCKET_SUBDIR=$2
   local IMAGE_FILE_NAME=$3
 
-  # validate other required values from the environment
+  # these env vars are required regardless of what creds are used
   echo "Validating incoming environment..."
-  if [ ! -v AWS_ARN_STRING ] || [ ! -v AWS_ACCESS_KEY_ID ] || [ ! -v AWS_SECRET_ACCESS_KEY ] || [ ! -v S3_BUCKET_NAME ] || [ ! -v S3_BUCKET_REGION ]; then
+  if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${S3_BUCKET_NAME}" ] || [ -z "${S3_BUCKET_REGION}" ]; then
     echo "ERROR: The following values must be set in the environment:"
-    echo "  AWS_ARN_STRING"
+    echo "  AWS_ARN_STRING (optional)"
     echo "  AWS_ACCESS_KEY_ID"
     echo "  AWS_SECRET_ACCESS_KEY"
     echo "  S3_BUCKET_NAME"
     echo "  S3_BUCKET_REGION"
+    echo "Keys must either be valid for direct access to the bucket, or valid for an assume-role operation if AWS_ARN_STRING is set"
     exit 1
   fi
 
@@ -62,18 +63,22 @@ fetch_docker_image_from_s3() {
   # ensure region is set
   export AWS_REGION=${S3_BUCKET_REGION}
 
-  # ask for temporary credentials for file access
-  echo "Requesting temporary S3 credentials..."
-  local TEMP_CREDS_JSON=$(aws sts assume-role \
-    --role-arn ${AWS_ARN_STRING} \
-    --role-session-name "GetPrestoContainerImage" \
-    --query "Credentials" \
-    --output json)
+  # if AWS_ARN_STRING is set in the environment, use environment creds to request new
+  # temporary rolling creds for the private bucket, otherwise use environment creds directly
+  if [ ! -z "${AWS_ARN_STRING}" ]; then
+    # ask for temporary credentials for file access
+    echo "Requesting temporary S3 credentials..."
+    local TEMP_CREDS_JSON=$(aws sts assume-role \
+      --role-arn ${AWS_ARN_STRING} \
+      --role-session-name "GetPrestoContainerImage" \
+      --query "Credentials" \
+      --output json)
 
-  # override environment with full temporary credentials
-  export AWS_ACCESS_KEY_ID=$(echo "$TEMP_CREDS_JSON" | jq -r '.AccessKeyId')
-  export AWS_SECRET_ACCESS_KEY=$(echo "$TEMP_CREDS_JSON" | jq -r '.SecretAccessKey')
-  export AWS_SESSION_TOKEN=$(echo "$TEMP_CREDS_JSON" | jq -r '.SessionToken')
+    # override environment with full temporary credentials
+    export AWS_ACCESS_KEY_ID=$(echo "$TEMP_CREDS_JSON" | jq -r '.AccessKeyId')
+    export AWS_SECRET_ACCESS_KEY=$(echo "$TEMP_CREDS_JSON" | jq -r '.SecretAccessKey')
+    export AWS_SESSION_TOKEN=$(echo "$TEMP_CREDS_JSON" | jq -r '.SessionToken')
+  fi
 
   # pull the repo image
   echo "Fetching image file from S3..."
