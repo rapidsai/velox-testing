2b98ca450e31e27c1cf07b9e39d61a44dce484f2
misiugodfrey <misiug@nvidia.com>
2025-10-17 15:42:10 -0700

    moved LOCAL_CONDA_INIT into init_conda()

--- scripts/py_env_functions.sh
+++ scripts/py_env_functions.sh
@@ -42,6 +42,7 @@ function init_conda() {
     fi
   fi
   unset __conda_setup
+  LOCAL_CONDA_INIT=true # keep track of whether we set up the current conda environment
   echo "Conda initialization completed"
 }
 
@@ -65,7 +66,6 @@ function init_python_virtual_env() {
     fi
 
     init_conda
-    LOCAL_CONDA_INIT=true # keep track of whether we set up the current conda environment
 
     echo "Creating virtual environment using conda"
     conda create -q -y --prefix ".venv" python=3.12 > /dev/null
