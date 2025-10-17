70c34baf316d13bb66806392a058c784f07b5cad
Simon Eves <seves@nvidia.com>
2025-10-17 11:12:36 -0700

    Add --auto-detect option

--- presto/scripts/build_centos_deps_image.sh
+++ presto/scripts/build_centos_deps_image.sh
@@ -3,8 +3,9 @@
 set -e
 
 IMAGE_NAME='presto/prestissimo-dependency:centos9'
-VELOX_REPO='rapidsai/velox'
-VELOX_COMMIT='merged-prs'
+VELOX_REPO=''
+VELOX_COMMIT=''
+AUTO_DETECT=false
 NO_CACHE_ARG=''
 
 print_help() {
@@ -26,6 +27,7 @@ OPTIONS:
     -i, --image-name     Desired Docker Image name (default: presto/prestissimo-dependency:centos9)
     -r, --velox-repo     Velox repo to use (default: rapidsai/velox) (full URL prefix not required)
     -c, --velox-commit   Velox repo commit/branch to use (default: merged-prs)
+    -a, --auto-detect    Auto-detect Velox repo/commit from sibling Velox clone
     -n, --no-cache       Do not use Docker build cache (default: use cache)
 
 EXAMPLES:
@@ -72,6 +74,10 @@ parse_args() {
         NO_CACHE_ARG="--no-cache"
         shift
         ;;
+      -a|--auto-detect)
+        AUTO_DETECT=true
+        shift
+        ;;
       *)
         echo "Error: Unknown argument $1"
         print_help
@@ -82,6 +88,37 @@ parse_args() {
 }
 
 parse_args "$@"
+
+#
+# determine Velox repo/commit
+#
+
+if [[ "${AUTO_DETECT}" == "true" ]]; then
+  # if auto-detect, repo/commit should not be set
+  if [[ -n "${VELOX_REPO}" || -n "${VELOX_COMMIT}" ]]; then
+    echo "Error: --auto-detect cannot be used with --velox-repo or --velox-commit"
+    exit 1
+  fi
+  # try to auto-detect Velox repo/commit from sibling Velox clone
+  if [[ -d "../../../velox" ]]; then
+    echo "Auto-detecting Velox repo/commit from sibling Velox clone..."
+    pushd ../../../velox > /dev/null
+    VELOX_REPO=$(git status | awk '/Your branch/ { gsub(/['\''\.]/, "", $8); print $8 }')
+    VELOX_COMMIT=$(git rev-parse HEAD)
+    popd > /dev/null
+  else
+    echo "Error: Auto-detect enabled but sibling Velox clone not found."
+    exit 1
+  fi
+elif [[ -z "${VELOX_REPO}" || -z "${VELOX_COMMIT}" ]]; then
+  # if not auto-detect, both repo and commit must be set
+  echo "Error: Both --velox-repo and --velox-commit must be specified unless --auto-detect is used."
+  exit 1
+fi
+
+echo "Using image name   ${IMAGE_NAME}"
+echo "Using Velox repo   ${VELOX_REPO}"
+echo "Using Velox commit ${VELOX_COMMIT}"
 
 #
 # remove any existing image?
@@ -99,7 +136,7 @@ fi
 echo "Building Presto dependencies/run-time image..."
 
 # move to Presto clone
-pushd ../../../presto
+pushd ../../../presto > /dev/null
 
 # reset submodule
 echo "Resetting Velox submodule to specified version"
@@ -114,12 +151,12 @@ echo "No patches currently required"
 
 # now build
 echo "Building..."
-pushd presto-native-execution
+pushd presto-native-execution > /dev/null
 docker compose --progress plain build ${NO_CACHE_ARG} centos-native-dependency
-popd
+popd > /dev/null
 
 # return
-popd
+popd > /dev/null
 
 # done
 echo "Presto dependencies/run-time container image built!"
