9c1a1d5e4743b75ec6507a7cd021d53419f9aa3a
Simon Eves <seves@nvidia.com>
2025-10-20 18:33:57 -0700

    WIP

---
+++ presto/scripts/checkout_velox_to_match_presto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# get SHA of Presto Velox submodule
+pushd ../../presto/presto-native-execution/velox
+SHA=$(git rev-parse HEAD)
+popd
+
+# checkout sibling Velox to that SHA
+pushd ../../velox
+get checkout ${SHA}
+popd
