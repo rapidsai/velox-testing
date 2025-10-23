78ae52b0f10c51be9a8946df5d189fcad24084ed
misiugodfrey <misiug@nvidia.com>
2025-10-23 13:18:58 -0700

    Added casting, and error checking for pbench config script

--- presto/scripts/generate_presto_config.sh
+++ presto/scripts/generate_presto_config.sh
@@ -16,6 +16,20 @@
 
 set -euo pipefail
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+function echo_error {
+    echo -e "${RED}$1${NC}"
+    exit 1
+}
+
+function echo_success {
+    echo -e "${GREEN}$1${NC}"
+}
+
 if [ ! -x ../pbench/pbench ]; then
     echo "ERROR: generate_presto_config.sh script must only be run from presto:presto/scripts"
     exit 1
@@ -24,7 +38,7 @@ fi
 # get host values
 NPROC=`nproc`
 # lsmem will report in SI.  Make sure we get values in GB.
-RAM_GB=$(lsmem -b | grep "Total online memory" | awk '{print $4 / (1024*1024*1024)}')
+RAM_GB=$(lsmem -b | grep "Total online memory" | awk '{print int($4 / (1024*1024*1024)); }')
 
 echo "Generating Presto Config files for ${NPROC} CPU cores and ${RAM_GB}GB RAM"
 
@@ -66,4 +80,9 @@ EOF
 
 # run pbench to generate the config files
 # hide default pbench logging which goes to stderr so we only see any errors
-../../pbench/pbench genconfig -p params.json -t template generated 2>&1 | grep -v '\{\"level'
+if ../../pbench/pbench genconfig -p params.json -t template generated 2>&1 | grep '\{\"level":"error"'; then
+    echo_error "ERROR in pbench genconfig.  Configs were not generated successfully"
+    exit 1
+fi
+
+echo_success "Configs were generated successfully"
