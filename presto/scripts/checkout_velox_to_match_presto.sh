a9c8a0bd9c7096f9453c32437480415ff8d0c7c1
Simon Eves <seves@nvidia.com>
2025-10-23 11:36:56 -0700

    Debug

--- presto/scripts/checkout_velox_to_match_presto.sh
+++ presto/scripts/checkout_velox_to_match_presto.sh
@@ -6,11 +6,17 @@ echo "Checking out version of Velox which matches Presto pinned version"
 
 # get SHA of Presto Velox submodule
 pushd ../../../presto/presto-native-execution/velox
+pwd
+ls -l
 SHA=$(git rev-parse HEAD)
 popd
 
+echo "Presto pinned version is ${SHA}"
+
 # checkout sibling Velox to that SHA
 pushd ../../../velox
+pwd
+ls -l
 git checkout ${SHA}
 popd
 
