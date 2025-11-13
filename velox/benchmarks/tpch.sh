669ad78ddc5d8abd125449941fd9551479c30b1f
Avinash <avistylein3105@gmail.com>
2025-11-13 05:15:14 -0800

    reverting back the changes on tpch.sh script

--- velox/benchmarks/tpch.sh
+++ velox/benchmarks/tpch.sh
@@ -195,7 +195,6 @@ run_tpch_single_benchmark() {
       BENCHMARK_EXECUTABLE="$(get_tpch_benchmark_executable_path "$device_type")"
       CUDF_FLAGS=""
       VELOX_CUDF_FLAGS=""
-      VELOX_CUDF_ENABLED=false
       ;;
     "gpu")
       num_drivers=${NUM_DRIVERS:-4}
@@ -203,8 +202,7 @@ run_tpch_single_benchmark() {
       cudf_pass_read_limit=0
       BENCHMARK_EXECUTABLE="$(get_tpch_benchmark_executable_path "$device_type")"
       CUDF_FLAGS="--cudf_chunk_read_limit=${cudf_chunk_read_limit} --cudf_pass_read_limit=${cudf_pass_read_limit}"
-      VELOX_CUDF_FLAGS=""
-      VELOX_CUDF_ENABLED=true
+      VELOX_CUDF_FLAGS="--velox_cudf_memory_resource=async"
       ;;
   esac
   
@@ -301,4 +299,3 @@ check_tpch_benchmark_executable() {
          "Please rebuild Velox with GPU support and benchmarks enabled by running: ./build_velox.sh --gpu --benchmarks true"
     fi
 }
-
