0d3364736b2ddee43d81fc23935eb3f3d8c64505
misiugodfrey <misiug@nvidia.com>
2025-12-10 18:11:34 -0800

    Rewrite to dynamically generate multi-worker dockerfile

--- presto/scripts/stop_presto.sh
+++ presto/scripts/stop_presto.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-docker compose -f ../docker/docker-compose.java.yml -f ../docker/docker-compose.native-cpu.yml -f ../docker/docker-compose.native-gpu.yml down
+OVERRIDE=""
+[ -f ../docker/docker-compose.workers.override.yml ] && OVERRIDE="-f ../docker/docker-compose.workers.override.yml"
+
+docker compose -f ../docker/docker-compose.java.yml -f ../docker/docker-compose.native-cpu.yml -f ../docker/docker-compose.native-gpu.yml ${OVERRIDE} down
