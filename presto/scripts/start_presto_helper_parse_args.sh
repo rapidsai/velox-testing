bc14f7e64ec95a19fd6b8aba4dac648530c7e68e
Paul Aiyedun <53453937+paul-aiyedun@users.noreply.github.com>
2026-02-12 11:42:04 -0800

    WIP: Cleanup start scripts

--- presto/scripts/start_presto_helper_parse_args.sh
+++ presto/scripts/start_presto_helper_parse_args.sh
@@ -14,31 +14,19 @@ This script deploys a Presto server cluster (one coordinator node and one $VARIA
 
 OPTIONS:
     -h, --help           Show this help message.
-    -n, --no-cache       Do not use the builder cache when building the image.
-    -b, --build          Service type to build from source. Possible values are
-                         "coordinator" or "c", "worker" or "w", and "all" or "a".
-                         By default, services will be lazily built i.e. a build
-                         will only occur if there is no local image for the service.
-    -j, --num-threads    Number of threads to use when building the image (default is `nproc` / 2).
     -w, --num-workers    Number of GPU workers to start (GPU variant only).
     -g, --gpu-ids        Comma-delimited list of GPU device IDs to use (e.g., "0,1,3,5").
                          Must be used with --num-workers. If not specified, defaults to "0,1,...,N-1"
                          where N is the value from --num-workers (GPU variant only).
     --single-container   Launch multiple Presto servers in a single container (GPU variant only).
-    --build-type         Build type for native CPU and GPU image builds. Possible values are "release",
-                         "relwithdebinfo", or "debug". Values are case insensitive. The default value
-                         is "release".
-    --all-cuda-archs     Build for all supported CUDA architectures (GPU only) (default: false).
     -p, --profile        Launch the Presto server with profiling enabled.
     --profile-args       Arguments to pass to the profiler when it launches the Presto server.
                          This will override the default arguments.
     --overwrite-config   Force config to be regenerated (will overwrite local changes).
 
 EXAMPLES:
-    $SCRIPT_NAME --no-cache
     $SCRIPT_NAME -b worker
     $SCRIPT_NAME --build c
-    $SCRIPT_NAME -j 8
     $SCRIPT_NAME -w 4
     $SCRIPT_NAME -w 4 -g 4,5,6,7
     $SCRIPT_NAME --profile
@@ -47,9 +35,6 @@ EXAMPLES:
 EOF
 }
 
-NUM_THREADS=$(($(nproc) / 2))
-BUILD_TYPE=release
-ALL_CUDA_ARCHS=false
 export SINGLE_CONTAINER=false
 export OVERWRITE_CONFIG=false
 export PROFILE=OFF
@@ -62,28 +47,6 @@ parse_args() {
       -h|--help)
         print_help
         exit 0
-        ;;
-      -n|--no-cache)
-        SKIP_CACHE_ARG="--no-cache"
-        shift
-        ;;
-      -b|--build)
-        if [[ -n $2 ]]; then
-          BUILD_TARGET=$2
-          shift 2
-        else
-          echo "Error: --build requires a value"
-          exit 1
-        fi
-        ;;
-      -j|--num-threads)
-        if [[ -n $2 ]]; then
-          NUM_THREADS=$2
-          shift 2
-        else
-          echo "Error: --num-threads requires a value"
-          exit 1
-        fi
         ;;
       --kvikio-threads)
         if [[ -n $2 ]]; then
@@ -125,16 +88,6 @@ parse_args() {
         export SINGLE_CONTAINER=true
         shift
         ;;
-      --build-type)
-        if [[ -n $2 ]]; then
-          # Convert value to lowercase using the "L" transformation operator.
-          BUILD_TYPE=${2@L}
-          shift 2
-        else
-          echo "Error: --build-type requires a value"
-          exit 1
-        fi
-        ;;
       -p|--profile)
         PROFILE=ON
         shift
@@ -147,10 +100,6 @@ parse_args() {
           echo "Error: --profile-args requires a value"
           exit 1
         fi
-        ;;
-      --all-cuda-archs)
-        ALL_CUDA_ARCHS=true
-        shift
         ;;
       --overwrite-config)
         OVERWRITE_CONFIG=true
@@ -166,24 +115,6 @@ parse_args() {
 }
 
 parse_args "$@"
-
-if [[ -n ${BUILD_TARGET} && ! ${BUILD_TARGET} =~ ^(coordinator|c|worker|w|all|a)$ ]]; then
-  echo "Error: invalid --build value."
-  print_help
-  exit 1
-fi
-
-if (( NUM_THREADS <= 0 )); then
-  echo "Error: --num-threads must be a positive integer."
-  print_help
-  exit 1
-fi
-
-if [[ ! ${BUILD_TYPE} =~ ^(release|relwithdebinfo|debug)$ ]]; then
-  echo "Error: invalid --build-type value."
-  print_help
-  exit 1
-fi
 
 if [[ -n $PROFILE_ARGS && "$PROFILE" == "OFF" ]]; then
   echo "Error: the --profile-args argument should only be set when --profile is enabled"
