ceb1f05ef9cdf90e8b238d57803a24d627a8e283
Avinash <avistylein3105@gmail.com>
2026-02-12 16:11:20 +0000

    replaced wildcard imports with explicit imports

--- presto/testing/integration_tests/conftest.py
+++ presto/testing/integration_tests/conftest.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
-from ..common.conftest import *  # noqa: F403
-from ..common.fixtures import *  # noqa: F403
-from .common_fixtures import *  # noqa: F403
+from ..common.conftest import pytest_generate_tests  # noqa: F401
+from ..common.fixtures import tpcds_queries, tpch_queries  # noqa: F401
+from .common_fixtures import presto_cursor, setup_and_teardown  # noqa: F401
 
 
 def pytest_addoption(parser):
