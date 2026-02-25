7d892c8096d5132078b7984f6fe5069927ede755
Bradley Dice <bdice@bradleydice.com>
2026-02-24 20:41:49 -0600

    Add copyright header

--- scripts/devcontainer/_devcontainer_common_utils.sh
+++ scripts/devcontainer/_devcontainer_common_utils.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # Shared constants and functions for velox/presto build scripts.
 # Source this file; do not execute directly.
 
