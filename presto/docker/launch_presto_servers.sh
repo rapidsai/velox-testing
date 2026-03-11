b1832ffcfdf651a00b1a88c7e3506e1fdfa7f58a
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2026-03-11 03:45:04 +0000

    enable NUMA
    install numactrl in worker images
    use LAUNCHER to do numactl cpubind, membind to numa node

--- presto/docker/launch_presto_servers.sh
+++ presto/docker/launch_presto_servers.sh
@@ -10,7 +10,7 @@ else
 # Launch workers in parallel, each pinned to a different GPU
 # The GPU IDs are passed as command-line arguments
 for gpu_id in "$@"; do
-  CUDA_VISIBLE_DEVICES=$gpu_id presto_server --etc-dir="/opt/presto-server/etc${gpu_id}" &
+  eval "CUDA_VISIBLE_DEVICES=$gpu_id ${LAUNCHER:-} presto_server --etc-dir='/opt/presto-server/etc${gpu_id}'" &
 done
 fi
 
