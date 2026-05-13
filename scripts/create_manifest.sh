1ef137f634456fada93ca6cd3f626dbd9ec75b7f
Bradley Dice <bdice@bradleydice.com>
2026-05-13 18:07:48 -0500

    Refactoring

--- scripts/create_manifest.sh
+++ scripts/create_manifest.sh
@@ -8,8 +8,14 @@ create_manifest_from_arch_tags() {
 
   echo "Creating multi-arch manifest: ${final_tag}"
   docker buildx imagetools create -t "${REGISTRY}/${IMAGE_NAME}:${final_tag}" \
-    "${REGISTRY}/${IMAGE_NAME}:${tag}${INTERMEDIATE_TAG_SUFFIX}-amd64" \
-    "${REGISTRY}/${IMAGE_NAME}:${tag}${INTERMEDIATE_TAG_SUFFIX}-arm64"
+    "${REGISTRY}/${IMAGE_NAME}:${tag}-${JOB_VARIANT_IDENTIFIER}-amd64" \
+    "${REGISTRY}/${IMAGE_NAME}:${tag}-${JOB_VARIANT_IDENTIFIER}-arm64"
+}
+
+resolve_run_id_suffix() {
+  if [[ "${GITHUB_EVENT_NAME}" == "workflow_dispatch" ]]; then
+    printf -- '-%s' "${GITHUB_RUN_ID_VALUE}"
+  fi
 }
 
 create_manifest() {
