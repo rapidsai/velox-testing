aa96422370b0e9221a109d30758cb3e91baaab8c
Bradley Dice <bdice@bradleydice.com>
2026-05-13 17:29:06 -0500

    Reuse manifest helper for latest tags

--- scripts/create_manifest.sh
+++ scripts/create_manifest.sh
@@ -2,13 +2,19 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-create_manifest() {
+create_manifest_from_arch_tags() {
   local tag="$1"
-  local run_id_suffix="${2:-}"
-  local final_tag="${tag}${run_id_suffix}"
+  local final_tag="$2"
 
   echo "Creating multi-arch manifest: ${final_tag}"
   docker buildx imagetools create -t "${REGISTRY}/${IMAGE_NAME}:${final_tag}" \
     "${REGISTRY}/${IMAGE_NAME}:${tag}${INTERMEDIATE_TAG_SUFFIX}-amd64" \
     "${REGISTRY}/${IMAGE_NAME}:${tag}${INTERMEDIATE_TAG_SUFFIX}-arm64"
+}
+
+create_manifest() {
+  local tag="$1"
+  local run_id_suffix="${2:-}"
+
+  create_manifest_from_arch_tags "${tag}" "${tag}${run_id_suffix}"
 }
