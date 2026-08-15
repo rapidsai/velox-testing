6f5756f858f9314eebaef99038f8857c627a51d0
Tianyu Liu <tialiu@nvidia.com>
2026-08-15 20:06:16 +0000

    Update

---
+++ presto/scripts/biu/cleanup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p /opt/dlami/nvme/presto_logs /opt/dlami/nvme/presto-gpu-nvme-cache
+sudo rm -rf /opt/dlami/nvme/presto_logs/* /opt/dlami/nvme/presto-gpu-nvme-cache/*
+
+cd ..
+rm -rf benchmark_output presto_logs
+
