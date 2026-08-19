ec9bb8dc8a8b24f559516a8c86bd50ba61ff5ce0
Tianyu Liu <tialiu@nvidia.com>
2026-08-19 14:00:28 +0000

    Update

--- presto/scripts/biu/run_bench.sh
+++ presto/scripts/biu/run_bench.sh
@@ -7,16 +7,20 @@ export AWS_DEFAULT_REGION=us-east-2
 
 export KVIKIO_TASK_SIZE=$((128 * 1024 * 1024))
 export KVIKIO_BOUNCE_BUFFER_SIZE=$((128 * 1024 * 1024))
-export KVIKIO_REMOTE_IO_BACKEND=EASY_THREADPOOL
-# export KVIKIO_REMOTE_IO_BACKEND=MULTI_POLL
+# export KVIKIO_REMOTE_IO_BACKEND=EASY_THREADPOOL
+export KVIKIO_REMOTE_IO_BACKEND=MULTI_POLL
 export KVIKIO_REMOTE_IO_NUM_REACTORS=16
 export KVIKIO_REMOTE_IO_MAX_CONCURRENT_REQUESTS=256
+export LIBCUDF_NUM_HOST_WORKERS=8
 
-#-b worker
 ./start_native_gpu_presto.sh --overwrite-config --kvikio-threads 160 --num-drivers 6 \
 --logs-dir /opt/dlami/nvme/presto_logs
+# -p --profile-args "-t nvtx,cuda --cuda-memory-usage=true"
+#-b worker
 
 # Reference data location: s3://rapids-tpch/presto-gpu/sf1k_v2_float/expected/
 ./run_benchmark.sh -b tpch -s tpch_sf1k_v2_float_s3 --reference-results-dir ~/rapids/reference_data/sf1k_v2_float
+# -i 2 # -q 1
+#-p
 
 cd -
